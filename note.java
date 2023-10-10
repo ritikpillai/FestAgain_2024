9272f11453a5037725b0e7552631606379beac76
Matteo Baccan <matteo.baccan@gmail.com>
2023-10-10 17:24:20 +0200

    Add maven build

--- note.java
+++
@@ -1,7 +0,0 @@
-// Updating Repo for Hacktoberfest 2023 
-class main {
-  public static void main(String[] arg){
-    
- System.out.println("hello world");
-  }
-}
---
+++ src/main/java/it/baccan/festagain/FestAgain.java
@@ -0,0 +1,13 @@
+package it.baccan.festagain;
+
+/**
+ * Updating Repo for Hacktoberfest 2023 
+ * @author Matteo Baccan
+ */
+public class FestAgain {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
