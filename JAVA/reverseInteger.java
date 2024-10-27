a371a2b74de2f5da936e18135c5539495011f5fa
Sanvi Rathore <rathoresanvi3@gmail.com>
2024-10-27 12:49:11 +0530

    added solution to reverse an integer problem

---
+++ JAVA/reverseInteger.java
@@ -0,0 +1,18 @@
+public class reverseInteger {
+    public static int reverse(int x) {
+        int rev =0;
+        while(x!=0){
+        int lastDigit = x%10;
+        if(rev<Integer.MIN_VALUE/10 || rev>Integer.MAX_VALUE/10){
+                return 0;
+        }
+        rev=(rev*10)+lastDigit;
+        x=x/10;
+        }
+    return rev;
+    }
+
+    public static void main(String[] args){
+        System.out.println(reverse(98));
+    }
+}
